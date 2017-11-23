70e49fa1f8b5d86914049adc6b2657e29f63a3d1
lankui <lankui@lankui-mac.local>
2017-11-23 22:41:35 +0800

    support the bytes transport

---
+++ core/Client.py
@@ -0,0 +1,59 @@
+# -*- coding: UTF-8 -*-
+
+import socket
+import struct
+import threading
+import time
+
+from core.Command import Command
+
+
+class Socket(object):
+    def __init__(self, ip, port):
+        self.__ip = ip
+        self.__port = port
+        self.__mutex = threading.Lock()
+        _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        _socket.connect((self.__ip, self.__port))
+        self.__socket = _socket
+        self.heartbeat()
+        self.__data = bytes()
+        self.__headerSize = 8
+        self.__delimiter = '\r\n'.encode('utf-8')
+
+    def receive(self):
+        self.__data += self.__socket.recv(1024)
+        if len(self.__data) < self.__headerSize:
+            return ''
+        command_type = struct.unpack("!i", self.__data[:4])[0]
+        data_length = struct.unpack("!i", self.__data[4:8])[0]
+        if len(self.__data) < self.__headerSize + data_length + len(self.__delimiter):
+            return ''
+        command_data = self.__data[self.__headerSize: self.__headerSize + data_length]
+        self.__data = self.__data[self.__headerSize + data_length + len(self.__delimiter):len(self.__data)]
+        command = Command()
+        command.set_type(command_type)
+        command.set_data_length(data_length)
+        command.set_data(command_data)
+        return command
+
+    def send_command(self, command):
+        self.__mutex.acquire()
+        self.__socket.send(struct.pack("!i", command.get_type()))
+        self.__socket.send(struct.pack("!i", command.get_data_length()))
+        self.__socket.send(command.get_data())
+        self.__socket.send(self.__delimiter)
+        self.__mutex.release()
+
+    def heartbeat(self):
+        t = threading.Thread(target=self.send_ping, name='HeartbeatThread')
+        t.start()
+
+    def send_ping(self):
+        while True:
+            command = Command()
+            command.set_type(3)
+            command.set_data_length(0)
+            command.set_data(bytes())
+            self.send_command(command)
+            time.sleep(20)
--- core/Runner.py
+++ core/Runner.py
@@ -1,27 +1,33 @@
 # -*- coding: UTF-8 -*-
 
+import struct
 import threading
 import time
 
 from app import QQMusic
-from core import Socket
+from core import Client
 
 
 class Runner(object):
     def __init__(self, ip, port):
-        self.__socket = Socket.Socket(ip, port)
+        self.__socket = Client.Socket(ip, port)
         self.__music = QQMusic.QQMusic()
 
     def start(self):
         while True:
             command = self.__socket.receive()
-            if command == 'pong':
+            if command == '':
+                pass
+            elif command.get_type() == 1:
                 print('Heartbeat: Pong')
+            elif command.get_type() == 0x7FFFFFFF:
+                print('Receive: OK')
             else:
-                print('Command: ' + command)
                 thread = threading.Thread(target=self.execute(command), name='ExecuteCommandThread')
                 thread.start()
             time.sleep(1)
 
     def execute(self, command):
-        self.__music.run(command)
+        if command.get_type() == 2 and command.get_data_length() > 0:
+            message = struct.unpack("!s", command.get_data())
+            self.__music.run(message)
