4097041deb498956e36f225c6a3799a727dc0df0
LanKui <lankui@lankui-mac.local>
2017-11-27 19:43:24 +0800

    create the thumbnail

--- core/Screen.py
+++ core/Screen.py
@@ -1,6 +1,6 @@
 import os
 
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 
 from core.Command import Command
 
@@ -10,17 +10,24 @@ class Screen(object):
         self.__command = command
         self.__client = client
         self.__screen_file = 'screen.jpg'
+        self.__screen_thumbnail_file = 'screen_thumbnail.jpg'
 
     def capture(self):
+        # create the screen image
         img = ImageGrab.grab()
         img = img.convert('RGB')
         img.save(self.__screen_file)
+        # create the thumbnail
+        img = Image.open(self.__screen_file)
+        w, h = img.size
+        img.thumbnail((w // 10, h // 10))
+        img.save(self.__screen_thumbnail_file)
         self.send()
 
     def send(self):
         command = Command()
         command.set_type(3)
-        command.set_data_length(os.path.getsize(self.__screen_file))
-        with open(self.__screen_file, 'rb') as f:
+        command.set_data_length(os.path.getsize(self.__screen_thumbnail_file))
+        with open(self.__screen_thumbnail_file, 'rb') as f:
             command.set_data(f.read())
         self.__client.send_command(command)
